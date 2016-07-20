f7157b51820113cd8c63608ff0ec0d2b74a623e9
rafrex <code@rafrex.com>
2016-07-19 22:37:44 -0700

    Add react-dev-opa boilerplate

---
+++ README.md
@@ -0,0 +1,3 @@
+# Detect It Live
+
+[Live detection test site for `detect-it`](http://detect-it.rafrex.com)
---
+++ index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+function App() {
+  return (
+    <div>
+      <div>Hello world!</div>
+    </div>
+  );
+}
+
+render(<App />, document.getElementById('root'));

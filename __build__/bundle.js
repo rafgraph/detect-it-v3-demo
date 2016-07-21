6e5fee584369e0b4e0f81a6d7055f7928a7e8d56
rafrex <code@rafrex.com>
2016-07-20 22:30:23 -0700

    Add live detection tests sans style

--- __build__/bundle.js
+++ __build__/bundle.js
@@ -55,16 +55,147 @@
 	
 	var _reactDom = __webpack_require__(/*! react-dom */ 33);
 	
+	var _detectIt = __webpack_require__(/*! detect-it */ 172);
+	
+	var _detectIt2 = _interopRequireDefault(_detectIt);
+	
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 	
 	function App() {
+	  function toString(item) {
+	    return item === undefined ? 'undefined' : item.toString();
+	  }
+	
+	  function testResult(item, prefix) {
+	    return _react2.default.createElement(
+	      'div',
+	      null,
+	      item,
+	      ': ',
+	      toString(prefix ? _detectIt2.default.state[prefix][item] : _detectIt2.default[item])
+	    );
+	  }
+	
 	  return _react2.default.createElement(
 	    'div',
 	    null,
 	    _react2.default.createElement(
+	      'h1',
+	      { id: 'detect-it' },
+	      'Detect It — Live Detection Test'
+	    ),
+	    _react2.default.createElement(
 	      'div',
 	      null,
-	      'Hello world!'
+	      _react2.default.createElement(
+	        'a',
+	        { href: 'https://github.com/rafrex/detect-it', target: '_blank' },
+	        'https://github.com/rafrex/detect-it'
+	      )
+	    ),
+	    _react2.default.createElement(
+	      'div',
+	      null,
+	      testResult('deviceType'),
+	      testResult('hasTouchEventsApi'),
+	      testResult('hasPointerEventsApi'),
+	      testResult('hasTouch'),
+	      testResult('maxTouchPoints'),
+	      testResult('primaryHover'),
+	      testResult('primaryPointer')
+	    ),
+	    _react2.default.createElement(
+	      'div',
+	      null,
+	      'Detect It\'s state is a deterministic function of the state of the four micro state machines that it contains:'
+	    ),
+	    _react2.default.createElement(
+	      'h2',
+	      { id: 'detect-hover' },
+	      'Detect Hover'
+	    ),
+	    _react2.default.createElement(
+	      'div',
+	      null,
+	      _react2.default.createElement(
+	        'a',
+	        { href: 'https://github.com/rafrex/detect-hover', target: '_blank' },
+	        'https://github.com/rafrex/detect-hover'
+	      )
+	    ),
+	    _react2.default.createElement(
+	      'div',
+	      null,
+	      testResult('hover', 'detectHover'),
+	      testResult('none', 'detectHover'),
+	      testResult('anyHover', 'detectHover'),
+	      testResult('anyNone', 'detectHover')
+	    ),
+	    _react2.default.createElement(
+	      'h2',
+	      { id: 'detect-pointer' },
+	      'Detect Pointer'
+	    ),
+	    _react2.default.createElement(
+	      'div',
+	      null,
+	      _react2.default.createElement(
+	        'a',
+	        { href: 'https://github.com/rafrex/detect-pointer', target: '_blank' },
+	        'https://github.com/rafrex/detect-pointer'
+	      )
+	    ),
+	    _react2.default.createElement(
+	      'div',
+	      null,
+	      testResult('fine', 'detectPointer'),
+	      testResult('coarse', 'detectPointer'),
+	      testResult('none', 'detectPointer'),
+	      testResult('anyFine', 'detectPointer'),
+	      testResult('anyCoarse', 'detectPointer'),
+	      testResult('anyNone', 'detectPointer')
+	    ),
+	    _react2.default.createElement(
+	      'h2',
+	      { id: 'detect-touch-events' },
+	      'Detect Touch Events'
+	    ),
+	    _react2.default.createElement(
+	      'div',
+	      null,
+	      _react2.default.createElement(
+	        'a',
+	        { href: 'https://github.com/rafrex/detect-touch-events', target: '_blank' },
+	        'https://github.com/rafrex/detect-touch-events'
+	      )
+	    ),
+	    _react2.default.createElement(
+	      'div',
+	      null,
+	      testResult('hasApi', 'detectTouchEvents'),
+	      testResult('maxTouchPoints', 'detectTouchEvents')
+	    ),
+	    _react2.default.createElement(
+	      'h2',
+	      { id: 'detect-pointer-events' },
+	      'Detect Pointer Events'
+	    ),
+	    _react2.default.createElement(
+	      'div',
+	      null,
+	      _react2.default.createElement(
+	        'a',
+	        { href: 'https://github.com/rafrex/detect-pointer-events', target: '_blank' },
+	        'https://github.com/rafrex/detect-pointer-events'
+	      )
+	    ),
+	    _react2.default.createElement(
+	      'div',
+	      null,
+	      testResult('hasApi', 'detectPointerEvents'),
+	      testResult('requiresPrefix', 'detectPointerEvents'),
+	      testResult('hasTouch', 'detectPointerEvents'),
+	      testResult('maxTouchPoints', 'detectPointerEvents')
 	    )
 	  );
 	}
@@ -21615,6 +21746,273 @@
 	var ReactMount = __webpack_require__(/*! ./ReactMount */ 164);
 	
 	module.exports = ReactMount.renderSubtreeIntoContainer;
+
+/***/ },
+/* 172 */
+/*!**********************************!*\
+  !*** ./~/detect-it/lib/index.js ***!
+  \**********************************/
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	
+	var _detectHover = __webpack_require__(/*! detect-hover */ 173);
+	
+	var _detectHover2 = _interopRequireDefault(_detectHover);
+	
+	var _detectPointer = __webpack_require__(/*! detect-pointer */ 174);
+	
+	var _detectPointer2 = _interopRequireDefault(_detectPointer);
+	
+	var _detectTouchEvents = __webpack_require__(/*! detect-touch-events */ 175);
+	
+	var _detectTouchEvents2 = _interopRequireDefault(_detectTouchEvents);
+	
+	var _detectPointerEvents = __webpack_require__(/*! detect-pointer-events */ 176);
+	
+	var _detectPointerEvents2 = _interopRequireDefault(_detectPointerEvents);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+	/*
+	 * detectIt object structure
+	 * const detectIt = {
+	 *   deviceType: 'mouseOnly' / 'touchOnly' / 'hybrid',
+	 *   hasTouchEventsApi: boolean,
+	 *   hasPointerEventsApi: boolean,
+	 *   hasTouch: boolean,
+	 *   maxTouchPoints: number,
+	 *   primaryHover: 'hover' / 'none',
+	 *   primaryPointer: 'fine' / 'coarse' / 'none',
+	 *   state: {
+	 *     detectHover,
+	 *     detectPointer,
+	 *     detectTouchEvents,
+	 *     detectPointerEvents,
+	 *   },
+	 *   update() {...},
+	 *   pointerEventsPrefix(value) {return value, value will only have prefix if requiresPrefix},
+	 * }
+	 */
+	
+	function robustMax(a, b) {
+	  function isNum(value) {
+	    return typeof value === 'number';
+	  }
+	  if (isNum(a) && isNum(b)) return Math.max(a, b);
+	  if (isNum(a)) return a;
+	  if (isNum(b)) return b;
+	  return undefined;
+	}
+	
+	function determineDeviceType(hasTouch, anyHover, anyFine) {
+	  /*
+	   * A hybrid device is one that both hasTouch and any input device can hover
+	   * or has a fine pointer.
+	   */
+	  if (hasTouch && (anyHover || anyFine)) return 'hybrid';
+	
+	  /*
+	   * In almost all cases a device that doesn’t support touch will have a mouse,
+	   * but there may be rare exceptions. Note that it doesn’t work to do additional tests
+	   * based on hover and pointer media queries as older browsers don’t support these.
+	   * Essentially, 'mouseOnly' is the default.
+	   */
+	  return hasTouch ? 'touchOnly' : 'mouseOnly';
+	}
+	
+	var detectIt = {
+	  state: {
+	    detectHover: _detectHover2.default,
+	    detectPointer: _detectPointer2.default,
+	    detectTouchEvents: _detectTouchEvents2.default,
+	    detectPointerEvents: _detectPointerEvents2.default
+	  },
+	  update: function update() {
+	    detectIt.state.detectHover.update();
+	    detectIt.state.detectPointer.update();
+	    detectIt.state.detectTouchEvents.update();
+	    detectIt.state.detectPointerEvents.update();
+	    detectIt.updateOnlyOwnProperties();
+	  },
+	  updateOnlyOwnProperties: function updateOnlyOwnProperties() {
+	    if (typeof window !== 'undefined') {
+	      detectIt.hasTouch = detectIt.state.detectTouchEvents.hasApi || detectIt.state.detectPointerEvents.hasTouch || false;
+	
+	      detectIt.deviceType = determineDeviceType(detectIt.hasTouch, detectIt.state.detectHover.anyHover, detectIt.state.detectPointer.anyFine);
+	
+	      detectIt.hasTouchEventsApi = detectIt.state.detectTouchEvents.hasApi;
+	      detectIt.hasPointerEventsApi = detectIt.state.detectPointerEvents.hasApi;
+	
+	      detectIt.maxTouchPoints = robustMax(detectIt.state.detectTouchEvents.maxTouchPoints, detectIt.state.detectPointerEvents.maxTouchPoints);
+	
+	      detectIt.primaryHover = detectIt.state.detectHover.hover && 'hover' || detectIt.state.detectHover.none && 'none' ||
+	      // if it's a mouseOnly device that doesn't support level 4 media queries,
+	      // then assume it hovers
+	      detectIt.deviceType === 'mouseOnly' && 'hover' ||
+	      // if it's a touchOnly device that doesn't support level 4 media queries,
+	      // then assume it doesn't hover, otherwise it's undefined
+	      detectIt.deviceType === 'touchOnly' && 'none' || undefined;
+	
+	      detectIt.primaryPointer = detectIt.state.detectPointer.fine && 'fine' || detectIt.state.detectPointer.coarse && 'coarse' || detectIt.state.detectPointer.none && 'none' ||
+	      // if it's a mouseOnly device that doesn't support level 4 media queries,
+	      // then assume it has a fine pointer
+	      detectIt.deviceType === 'mouseOnly' && 'fine' ||
+	      // if it's a touchOnly device that doesn't support level 4 media queries,
+	      // then assume it has a coarse pointer, otherwise it's undefined
+	      detectIt.deviceType === 'touchOnly' && 'coarse' || undefined;
+	    }
+	  },
+	
+	  pointerEventsPrefix: _detectPointerEvents2.default.prefix
+	};
+	
+	detectIt.updateOnlyOwnProperties();
+	exports.default = detectIt;
+
+/***/ },
+/* 173 */
+/*!*************************************!*\
+  !*** ./~/detect-hover/lib/index.js ***!
+  \*************************************/
+/***/ function(module, exports) {
+
+	'use strict';
+	
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	var detectHover = {
+	  update: function update() {
+	    if (typeof window !== 'undefined' && typeof window.matchMedia === 'function') {
+	      detectHover.hover = window.matchMedia('(hover: hover)').matches;
+	      detectHover.none = window.matchMedia('(hover: none)').matches || window.matchMedia('(hover: on-demand)').matches;
+	      detectHover.anyHover = window.matchMedia('(any-hover: hover)').matches;
+	      detectHover.anyNone = window.matchMedia('(any-hover: none)').matches || window.matchMedia('(any-hover: on-demand)').matches;
+	    }
+	  }
+	};
+	
+	detectHover.update();
+	exports.default = detectHover;
+
+/***/ },
+/* 174 */
+/*!***************************************!*\
+  !*** ./~/detect-pointer/lib/index.js ***!
+  \***************************************/
+/***/ function(module, exports) {
+
+	'use strict';
+	
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	var detectPointer = {
+	  update: function update() {
+	    if (typeof window !== 'undefined' && typeof window.matchMedia === 'function') {
+	      detectPointer.fine = window.matchMedia('(pointer: fine)').matches;
+	      detectPointer.coarse = window.matchMedia('(pointer: coarse)').matches;
+	      detectPointer.none = window.matchMedia('(pointer: none)').matches;
+	      detectPointer.anyFine = window.matchMedia('(any-pointer: fine)').matches;
+	      detectPointer.anyCoarse = window.matchMedia('(any-pointer: coarse)').matches;
+	      detectPointer.anyNone = window.matchMedia('(any-pointer: none)').matches;
+	    }
+	  }
+	};
+	
+	detectPointer.update();
+	exports.default = detectPointer;
+
+/***/ },
+/* 175 */
+/*!********************************************!*\
+  !*** ./~/detect-touch-events/lib/index.js ***!
+  \********************************************/
+/***/ function(module, exports) {
+
+	'use strict';
+	
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	var detectTouchEvents = {
+	  update: function update() {
+	    if (typeof window !== 'undefined') {
+	      detectTouchEvents.hasApi = 'ontouchstart' in window;
+	      detectTouchEvents.maxTouchPoints = detectTouchEvents.hasApi ? window.navigator && window.navigator.maxTouchPoints : undefined;
+	    }
+	  }
+	};
+	
+	detectTouchEvents.update();
+	exports.default = detectTouchEvents;
+
+/***/ },
+/* 176 */
+/*!**********************************************!*\
+  !*** ./~/detect-pointer-events/lib/index.js ***!
+  \**********************************************/
+/***/ function(module, exports) {
+
+	'use strict';
+	
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	var prefixMap = {
+	  pointerdown: 'MSPointerDown',
+	  pointerup: 'MSPointerUp',
+	  pointercancel: 'MSPointerCancel',
+	  pointermove: 'MSPointerMove',
+	  pointerover: 'MSPointerOver',
+	  pointerout: 'MSPointerOut',
+	  pointerenter: 'MSPointerEnter',
+	  pointerleave: 'MSPointerLeave',
+	  gotpointercapture: 'MSGotPointerCapture',
+	  lostpointercapture: 'MSLostPointerCapture',
+	  maxTouchPoints: 'msMaxTouchPoints'
+	};
+	
+	/*
+	 * detectPointerEvents object structure
+	 * const detectPointerEvents = {
+	 *   hasApi: boolean,
+	 *   requiresPrefix: boolean,
+	 *   hasTouch: boolean,
+	 *   maxTouchPoints: number,
+	 *   update() {...},
+	 *   prefix(value) {return value, value will only have prefix if requiresPrefix === true},
+	 * }
+	 */
+	var detectPointerEvents = {
+	  update: function update() {
+	    if (typeof window !== 'undefined') {
+	      if ('PointerEvent' in window) {
+	        detectPointerEvents.hasApi = true;
+	        detectPointerEvents.requiresPrefix = false;
+	      } else if (window.navigator && 'msPointerEnabled' in window.navigator) {
+	        detectPointerEvents.hasApi = true;
+	        detectPointerEvents.requiresPrefix = true;
+	      } else {
+	        detectPointerEvents.hasApi = false;
+	        detectPointerEvents.requiresPrefix = undefined;
+	      }
+	      detectPointerEvents.maxTouchPoints = detectPointerEvents.hasApi && window.navigator && window.navigator[detectPointerEvents.prefix('maxTouchPoints')] || undefined;
+	      detectPointerEvents.hasTouch = detectPointerEvents.hasApi ? detectPointerEvents.maxTouchPoints > 0 : undefined;
+	    }
+	  },
+	  prefix: function prefix(value) {
+	    return detectPointerEvents.requiresPrefix && prefixMap[value] || value;
+	  }
+	};
+	
+	detectPointerEvents.update();
+	exports.default = detectPointerEvents;
 
 /***/ }
 /******/ ]);
--- index.jsx
+++ index.jsx
@@ -1,10 +1,69 @@
 import React from 'react';
 import { render } from 'react-dom';
+import detectIt from 'detect-it';
 
 function App() {
+  function toString(item) {
+    return item === undefined ? 'undefined' : item.toString();
+  }
+
+  function testResult(item, prefix) {
+    return <div>{item}: {toString(prefix ? detectIt.state[prefix][item] : detectIt[item])}</div>;
+  }
+
   return (
     <div>
-      <div>Hello world!</div>
+      <h1 id="detect-it">Detect It &#8212; Live Detection Test</h1>
+      <div><a href="https://github.com/rafrex/detect-it" target="_blank">https://github.com/rafrex/detect-it</a></div>
+      <div>
+        {testResult('deviceType')}
+        {testResult('hasTouchEventsApi')}
+        {testResult('hasPointerEventsApi')}
+        {testResult('hasTouch')}
+        {testResult('maxTouchPoints')}
+        {testResult('primaryHover')}
+        {testResult('primaryPointer')}
+      </div>
+      <div>
+        Detect It's state is a deterministic function of the state of the four
+        micro state machines that it contains:
+      </div>
+
+      <h2 id="detect-hover">Detect Hover</h2>
+      <div><a href="https://github.com/rafrex/detect-hover" target="_blank">https://github.com/rafrex/detect-hover</a></div>
+      <div>
+        {testResult('hover', 'detectHover')}
+        {testResult('none', 'detectHover')}
+        {testResult('anyHover', 'detectHover')}
+        {testResult('anyNone', 'detectHover')}
+      </div>
+
+      <h2 id="detect-pointer">Detect Pointer</h2>
+      <div><a href="https://github.com/rafrex/detect-pointer" target="_blank">https://github.com/rafrex/detect-pointer</a></div>
+      <div>
+        {testResult('fine', 'detectPointer')}
+        {testResult('coarse', 'detectPointer')}
+        {testResult('none', 'detectPointer')}
+        {testResult('anyFine', 'detectPointer')}
+        {testResult('anyCoarse', 'detectPointer')}
+        {testResult('anyNone', 'detectPointer')}
+      </div>
+
+      <h2 id="detect-touch-events">Detect Touch Events</h2>
+      <div><a href="https://github.com/rafrex/detect-touch-events" target="_blank">https://github.com/rafrex/detect-touch-events</a></div>
+      <div>
+        {testResult('hasApi', 'detectTouchEvents')}
+        {testResult('maxTouchPoints', 'detectTouchEvents')}
+      </div>
+
+      <h2 id="detect-pointer-events">Detect Pointer Events</h2>
+      <div><a href="https://github.com/rafrex/detect-pointer-events" target="_blank">https://github.com/rafrex/detect-pointer-events</a></div>
+      <div>
+        {testResult('hasApi', 'detectPointerEvents')}
+        {testResult('requiresPrefix', 'detectPointerEvents')}
+        {testResult('hasTouch', 'detectPointerEvents')}
+        {testResult('maxTouchPoints', 'detectPointerEvents')}
+      </div>
     </div>
   );
 }

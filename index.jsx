f499ad902dcff4e75fd31897017cd35e084c0c2d
rafrex <code@rafrex.com>
2016-11-14 13:20:13 -0800

    Update for Detect It v2

--- index.jsx
+++ index.jsx
@@ -18,13 +18,12 @@ function App() {
   return (
     <div style={styles.app}>
       <h1 id="detect-it" style={styles.h1}>Detect It &#8212; Live Detection Test</h1>
-      <div style={styles.repoLink}><a href="https://github.com/rafrex/detect-it" target="_blank">https://github.com/rafrex/detect-it</a></div>
+      <div style={styles.repoLink}><a href="https://github.com/rafrex/detect-it" target="_blank" rel="noopener noreferrer">https://github.com/rafrex/detect-it</a></div>
       <div style={styles.code}>
         <div style={styles.detects}>
           {testResult('deviceType')}
           {testResult('passiveEvents')}
-          {testResult('hasTouchEventsApi')}
-          {testResult('hasPointerEventsApi')}
+          {testResult('hasMouse')}
           {testResult('hasTouch')}
           {testResult('maxTouchPoints')}
           {testResult('primaryHover')}
@@ -34,7 +33,7 @@ function App() {
 
 
       <h2 id="detect-hover" style={styles.h2}>Detect Hover</h2>
-      <div style={styles.repoLink}><a href="https://github.com/rafrex/detect-hover" target="_blank">https://github.com/rafrex/detect-hover</a></div>
+      <div style={styles.repoLink}><a href="https://github.com/rafrex/detect-hover" target="_blank" rel="noopener noreferrer">https://github.com/rafrex/detect-hover</a></div>
       <div style={styles.code}>
         <div style={styles.detects}>
           {testResult('hover', 'detectHover')}
@@ -45,7 +44,7 @@ function App() {
       </div>
 
       <h2 id="detect-pointer" style={styles.h2}>Detect Pointer</h2>
-      <div style={styles.repoLink}><a href="https://github.com/rafrex/detect-pointer" target="_blank">https://github.com/rafrex/detect-pointer</a></div>
+      <div style={styles.repoLink}><a href="https://github.com/rafrex/detect-pointer" target="_blank" rel="noopener noreferrer">https://github.com/rafrex/detect-pointer</a></div>
       <div style={styles.code}>
         <div style={styles.detects}>
           {testResult('fine', 'detectPointer')}
@@ -58,7 +57,7 @@ function App() {
       </div>
 
       <h2 id="detect-touch-events" style={styles.h2}>Detect Touch Events</h2>
-      <div style={styles.repoLink}><a href="https://github.com/rafrex/detect-touch-events" target="_blank">https://github.com/rafrex/detect-touch-events</a></div>
+      <div style={styles.repoLink}><a href="https://github.com/rafrex/detect-touch-events" target="_blank" rel="noopener noreferrer">https://github.com/rafrex/detect-touch-events</a></div>
       <div style={styles.code}>
         <div style={styles.detects}>
           {testResult('hasApi', 'detectTouchEvents')}
@@ -66,26 +65,15 @@ function App() {
         </div>
       </div>
 
-      <h2 id="detect-pointer-events" style={styles.h2}>Detect Pointer Events</h2>
-      <div style={styles.repoLink}><a className="test" href="https://github.com/rafrex/detect-pointer-events" target="_blank">https://github.com/rafrex/detect-pointer-events</a></div>
-      <div style={styles.code}>
-        <div style={styles.detects}>
-          {testResult('hasApi', 'detectPointerEvents')}
-          {testResult('requiresPrefix', 'detectPointerEvents')}
-          {testResult('hasTouch', 'detectPointerEvents')}
-          {testResult('maxTouchPoints', 'detectPointerEvents')}
-        </div>
-      </div>
-
       <h2 id="detect-passive-events" style={styles.h2}>Detect Passive Events</h2>
-      <div style={styles.repoLink}><a className="test" href="https://github.com/rafrex/detect-passive-events" target="_blank">https://github.com/rafrex/detect-passive-events</a></div>
+      <div style={styles.repoLink}><a className="test" href="https://github.com/rafrex/detect-passive-events" target="_blank" rel="noopener noreferrer">https://github.com/rafrex/detect-passive-events</a></div>
       <div style={styles.code}>
         <div style={styles.detects}>
           {testResult('hasSupport', 'detectPassiveEvents')}
         </div>
       </div>
 
-      <div style={styles.creditLink}><a className="extended-link" href="http://www.rafaelpedicini.com" target="_blank">Code and concept by <span>Rafael Pedicini</span></a></div>
+      <div style={styles.creditLink}><a className="extended-link" href="http://www.rafaelpedicini.com" target="_blank" rel="noopener noreferrer">Code and concept by <span>Rafael Pedicini</span></a></div>
     </div>
   );
 }
